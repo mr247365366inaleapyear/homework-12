2404b913e2ab313ab4b3e6cd3a0369d114e77787
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-18 12:25:05 -0500

    creating tables

--- database.sql
+++ database.sql
@@ -4,9 +4,20 @@ CREATE DATABASE EmployeeTracker;
 
 USE EmployeeTracker;
 
-CREATE TABLE employee();
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    employee_role varchar(30) not null,
+    employee_id integer not null,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE role();
+CREATE TABLE role(
+        id INTEGER AUTO_INCREMENT NOT NULL,
+
+
+);
 
 CREATE TABLE department();
 
