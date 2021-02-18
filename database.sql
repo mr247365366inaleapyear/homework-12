7fd7575d6fe2e47d18d2121fee3491a94cea672c
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-18 12:16:06 -0500

    outlined database

---
+++ database.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS EmployeeTracker;
+
+CREATE DATABASE EmployeeTracker;
+
+USE EmployeeTracker;
+
+CREATE TABLE employee();
+
+CREATE TABLE role();
+
+CREATE TABLE department();
+
+select * from employee;
+select * from role;
+select * from department;
+
