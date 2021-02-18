83e1f541fd7fa3b81fc5d0249e6fb07809cb2e3f
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-18 12:34:52 -0500

    adding data

--- database.sql
+++ database.sql
@@ -20,9 +20,22 @@ CREATE TABLE role(
         PRIMARY KEY (id)
 );
 
-CREATE TABLE department();
+CREATE TABLE department(
+        id INTEGER AUTO_INCREMENT NOT NULL,
+        department_name varchar(30),
+        PRIMARY KEY (id)
+
+);
 
 select * from employee;
 select * from role;
 select * from department;
 
+INSERT into employee (first_name, last_name, title, employee_id)
+values ("Nino", "Brown", CEO, 1);
+INSERT into employee (first_name, last_name, title, employee_id)
+values ("Luka", "Broski", Enforcer, 8);
+INSERT into employee (first_name, last_name, title, employee_id)
+values ("Duh-Duh", "Man", Enforcer, 8);
+INSERT into employee (first_name, last_name, title, employee_id)
+values ("Tom", "Brady", Cheater, 12);
