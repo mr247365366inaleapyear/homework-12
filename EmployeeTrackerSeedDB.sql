737a7f4d33aaeb044add9eff567a804dbdb64a1a
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-19 03:10:23 -0500

    edited seeds

--- EmployeeTrackerSeedDB.sql
+++ EmployeeTrackerSeedDB.sql
@@ -5,15 +5,23 @@ select * from employee;
 select * from role;
 select * from department;
 
-INSERT into department select (department_name);
-select ("Kingpin");
-INSERT into department select (department_name);
-select ("Bodyguard");
-INSERT into department select (department_name);
-select ("NFL");
+INSERT INTO department select (department_name)
+VALUES ("Kingpin");
+INSERT INTO department select (department_name)
+VALUES ("Bodyguard");
+INSERT INTO department select (department_name)
+VALUES ("NFL");
 
 select * from department;
 
+INSERT into role (title, salary)
+VALUES (CEO, 52000000);
+INSERT into role (title, salary)
+VALUES (Enforcer, 70000);
+INSERT into role (title, salary)
+VALUES (Cheater, 30000000);
+
+select * from role;
 
 INSERT into employee (first_name, last_name, title, employee_id)
 VALUES ("Nino", "Brown", CEO, 1);
@@ -25,12 +33,3 @@ INSERT into employee (first_name, last_name, title, employee_id)
 VALUES ("Tom", "Brady", Cheater, 12);
 
 select * from employee;
-
-INSERT into role (title, salary)
-VALUES (CEO, 52000000);
-INSERT into role (title, salary)
-VALUES (Enforcer, 70000);
-INSERT into role (title, salary)
-VALUES (Cheater, 30000000);
-
-select * from role;
