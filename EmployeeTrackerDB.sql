50a3509716981b45c4d9b19baeb71947d19b5d15
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-19 02:55:23 -0500

    fixed errors in database on execution

--- EmployeeTrackerDB.sql
+++ EmployeeTrackerDB.sql
@@ -5,25 +5,28 @@ CREATE DATABASE EmployeeTrackerDB;
 USE EmployeeTrackerDB;
 
 CREATE TABLE department(
-        id INT NOT NULL AUTO_INCREMENT,
-        department_name varchar(30),
-        PRIMARY KEY (id)
+
+	id INT NOT NULL AUTO_INCREMENT,
+	department_name varchar(30) NOT NULL,
+	PRIMARY KEY (id)
 
 );
+CREATE TABLE role (
 
-CREATE TABLE role(
-        id INT NOT NULL AUTO_INCREMENT,
-        title varchar(30) not null,
-        salary decimal not null,
-        department_id integer not null
-        PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  title varchar(30) NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+  
 );
+CREATE TABLE employee (
 
-CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name varchar(30) not null,
-    last_name varchar(30) not null,
-    role_id varchar(30) not null,
-    manager_id integer
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id varchar(30) NOT null,
+  manager_id integer,
+  PRIMARY KEY (id)
+  
 );
