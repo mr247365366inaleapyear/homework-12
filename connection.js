c7f743e6169427ab4cf890e6e4d05ea23079a5f1
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-19 03:47:06 -0500

    connection.js

---
+++ connection.js
@@ -0,0 +1,7 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const table = require('console.table');
+
+module.exports = mysql;
+module.exports = inquirer;
+module.exports = table;
