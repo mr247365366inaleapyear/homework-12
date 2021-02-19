92dd5b265a118da8436f1f449e8cbf48b838b048
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-19 14:12:41 -0500

    connection.js

--- connection.js
+++ connection.js
@@ -1,15 +1,34 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const table = require('console.table');
+var express = require('express'),
 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3000,
     user: "root",
-    password: "Cartel",
+    password: "",
     database: "EmployeeTrackerDB"
-})
+});
+connection.connect(function(error) {
+    if(!!error) {
+        console.log('Error');
+    } else {
+        console.log('Connected');
+    }
+});
 
+app.get('/', function(req, resp) {
+    connection.query("SELECT * FROM EmployeeTrackerDB", function(error, rows, fields)
+    if (error) {
+        console.log('Error in the query');
+    } else {
+        console.log('Successful ')
+    }
+});
+
+
+app.listen(1337);
 //module.exports = mysql;
 module.exports = inquirer;
 module.exports = table;
