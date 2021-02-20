3af6c8a5a5cb54ed6e9dcbab7202cf0e616f2b78
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-20 18:06:40 -0500

    redo code for functioning

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql');
-//const inquirer = require('inquirer');
-//const table = require('console.table');
-//const express = require('express'),
+/*const mysql = require('mysql');
+const inquirer = require('inquirer');
+const table = require('console.table');
+const express = require('express'),
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -17,10 +17,28 @@ connection.connect(function (err) {
         //console.log('Connected');
     //}
     console.log("connected as id " + connection.threadId);
-    questions();
 });
 
 //module.exports = express;
 //module.exports = inquirer;
 //module.exports = table;
 module.exports = connection;
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+// const sql = require("./sql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Take!2over",
+  database: "EmployeeTrackerDB"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    firstPrompt();
+  });
+
+  module.exports = connection;*/
